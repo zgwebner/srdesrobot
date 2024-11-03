e6b887356fdde1df062e3a47aabb2b3ab130b439
zgwebner <zgwebner@gmail.com>
2024-11-02 22:07:15 -0400

    Structure

---
+++ main/controller.py
@@ -0,0 +1,14 @@
+
+
+
+def main():
+	while True:
+		events = inputs.get_gamepad()
+		for event in events:
+			if str(event.ev_type) == "Absolute" or str(event.ev_type) == "Key":
+				print("event code " + str(event.code))
+				print("event state " + str(event.state))
+				print("----")
+if __name__ == "__main__":
+	main()
+
